7636e4d013778d7129fba62d4a754959357b9cf7
prasadambasaveswar <basaveswar01@gmail.com>
2018-12-20 19:31:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,19 +28,18 @@ resource "aws_s3_bucket_notification" "bucket_notification_new_sns" {
 resource "aws_sns_topic" "s3-topic" {
   count = "${var.create_s3_notification && var.create_sns_notification ? 1 : 0}"
   name = "${var.sns_topic_name}"
-  policy = <<POLICY
+   policy = <<POLICY
 {
-    "Version":"2008-10-17",
-    "Statement":[
-      {
+    "Version":"2012-10-17",
+    "Statement":[{
         "Effect": "Allow",
-        "Principal": {"AWS": "*"},
+        "Principal": {"AWS":"*"},
         "Action": "SNS:Publish",
-        "Resource": ${jsonencode(var.sns_topic_resource)},
+        "Resource": "arn:aws:sns:*:*:${var.sns_topic_resource}",
         "Condition":{
-            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.s3_bucket.arn}"}
+            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.egress-bucket.arn}"}
         }
-      } ]
+    }]
 }
 POLICY
 }
